852f93fea1d72731e92b6672bc6311cffb1d3b4b
KJABIRAMY <164976137+KJABIRAMY@users.noreply.github.com>
2024-07-24 12:57:08 +0530

    Add files via upload

---
+++ MatrixTranspose.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+class MatrixTranspose {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        System.out.println("Transpose Matrix:");
+        printMatrix(transpose);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
